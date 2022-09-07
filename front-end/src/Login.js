b985aaa0e05b22731df5f8ab87e9264df603177b
ziqian pan <ziqianpan1998@gmail.com>
2022-09-07 01:37:41 -0400

    cahnged

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 const CLIENT_ID = process.env.CLIENT_ID
 
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?CLIENT_ID=b52a6ccefe1f4faf833b99dbc183c142&response_type=code&redirect_uri=https://shimmering-sherbet-dc21b4.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=https://shimmering-sherbet-dc21b4.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
   return (
