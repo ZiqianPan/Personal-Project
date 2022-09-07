59ebb06d293bc5b77e78802dd55fc8e38318346a
ziqian pan <ziqianpan1998@gmail.com>
2022-09-06 23:58:28 -0400

    change port

--- back-end/server.js
+++ back-end/server.js
@@ -4,11 +4,11 @@ const app = require("./app.js");
 // CONFIGURATION
 require("dotenv").config();
 
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-// LISTEN
-// app.listen(PORT, () => {
-//   console.log(`🥤 🍿 Snackin' on port ${PORT} 🥨 🌰 `);
-// });
+LISTEN
+app.listen(PORT, () => {
+  console.log(`🥤 🍿 Snackin' on port ${PORT} 🥨 🌰 `);
+});
 
-app.listen(3001);
+// app.listen(3001);
